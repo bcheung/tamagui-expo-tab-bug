19e4586625b50db62aad96b1508a905691f5c41f
Brian Cheung <bcheung.dev@gmail.com>
2024-12-14 03:11:48 -0600

    add tabs based on docs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,55 +1,159 @@
-import { ExternalLink } from "@tamagui/lucide-icons";
-import { Anchor, H2, Paragraph, XStack, YStack } from "tamagui";
+import React from "react";
+import type { TabsContentProps } from "tamagui";
+import {
+  Button,
+  H2,
+  H5,
+  Separator,
+  SizableText,
+  Tabs,
+  XStack,
+  YStack,
+  isWeb,
+} from "tamagui";
 import { ToastControl } from "app/CurrentToast";
 
-export default function TabOneScreen() {
-  return (
-    <YStack f={1} ai="center" gap="$8" px="$10" pt="$5" bg="$background">
-      <H2>Tamagui + Expo</H2>
+const demos = ["horizontal", "vertical"] as const;
+const demosTitle: Record<(typeof demos)[number], string> = {
+  horizontal: "Horizontal",
+  vertical: "Vertical",
+};
 
+export default function TabOneScreen() {
+  const [demoIndex, setDemoIndex] = React.useState(0);
+  const demo = demos[demoIndex];
+
+  return (
+    <YStack
+      f={1}
+      ai="center"
+      gap="$8"
+      px="$10"
+      pt="$5"
+      bg="$background"
+      {...(isWeb && {
+        position: "unset" as any,
+      })}
+    >
+      <H2>Tamagui + Expo</H2>
       <ToastControl />
 
+      {demo === "horizontal" ? <HorizontalTabs /> : <VerticalTabs />}
+
       <XStack
-        ai="center"
-        jc="center"
-        fw="wrap"
-        gap="$1.5"
-        pos="absolute"
-        b="$8"
+        alignItems="center"
+        position="absolute"
+        bottom="$3"
+        left="$4"
+        $xxs={{ display: "none" }}
       >
-        <Paragraph fos="$5">Add</Paragraph>
-
-        <Paragraph fos="$5" px="$2" py="$1" col="$blue10" bg="$blue5">
-          tamagui.config.ts
-        </Paragraph>
-
-        <Paragraph fos="$5">to root and follow the</Paragraph>
-
-        <XStack
-          ai="center"
-          gap="$1.5"
-          px="$2"
-          py="$1"
-          br="$3"
-          bg="$purple5"
-          hoverStyle={{ bg: "$purple6" }}
-          pressStyle={{ bg: "$purple4" }}
+        <Button
+          size="$2"
+          onPress={() => setDemoIndex((x) => (x + 1) % demos.length)}
         >
-          <Anchor
-            href="https://tamagui.dev/docs/core/configuration"
-            textDecorationLine="none"
-            col="$purple10"
-            fos="$5"
-          >
-            Configuration guide
-          </Anchor>
-          <ExternalLink size="$1" col="$purple10" />
-        </XStack>
-
-        <Paragraph fos="$5" ta="center">
-          to configure your themes and tokens.
-        </Paragraph>
+          {demosTitle[demo]}
+        </Button>
       </XStack>
     </YStack>
   );
 }
+
+const HorizontalTabs = () => {
+  return (
+    <Tabs
+      defaultValue="tab1"
+      orientation="horizontal"
+      flexDirection="column"
+      width={400}
+      height={150}
+      borderRadius="$4"
+      borderWidth="$0.25"
+      overflow="hidden"
+      borderColor="$borderColor"
+    >
+      <Tabs.List
+        separator={<Separator vertical />}
+        disablePassBorderRadius="bottom"
+        aria-label="Manage your account"
+      >
+        <Tabs.Tab flex={1} value="tab1">
+          <SizableText fontFamily="$body">Profile</SizableText>
+        </Tabs.Tab>
+        <Tabs.Tab flex={1} value="tab2">
+          <SizableText fontFamily="$body">Connections</SizableText>
+        </Tabs.Tab>
+        <Tabs.Tab flex={1} value="tab3">
+          <SizableText fontFamily="$body">Notifications</SizableText>
+        </Tabs.Tab>
+      </Tabs.List>
+      <Separator />
+      <TabsContent value="tab1">
+        <H5>Profile</H5>
+      </TabsContent>
+      <TabsContent value="tab2">
+        <H5>Connections</H5>
+      </TabsContent>
+      <TabsContent value="tab3">
+        <H5>Notifications</H5>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+const VerticalTabs = () => {
+  return (
+    <Tabs
+      defaultValue="tab1"
+      flexDirection="row"
+      orientation="vertical"
+      width={400}
+      borderRadius="$4"
+      borderWidth="$0.25"
+      overflow="hidden"
+      borderColor="$borderColor"
+    >
+      <Tabs.List disablePassBorderRadius="end" aria-label="Manage your account">
+        <Tabs.Tab value="tab1">
+          <SizableText>Profile</SizableText>
+        </Tabs.Tab>
+        <Tabs.Tab value="tab2">
+          <SizableText>Connections</SizableText>
+        </Tabs.Tab>
+        <Tabs.Tab value="tab3">
+          <SizableText>Notifications</SizableText>
+        </Tabs.Tab>
+      </Tabs.List>
+      <Separator vertical />
+      <TabsContent value="tab1">
+        <H5 textAlign="center">Profile</H5>
+      </TabsContent>
+      <TabsContent value="tab2">
+        <H5 textAlign="center">Connections</H5>
+      </TabsContent>
+      <TabsContent value="tab3">
+        <H5 textAlign="center">Notifications</H5>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+const TabsContent = (props: TabsContentProps) => {
+  return (
+    <Tabs.Content
+      backgroundColor="$background"
+      key="tab3"
+      padding="$2"
+      alignItems="center"
+      justifyContent="center"
+      flex={1}
+      borderColor="$background"
+      borderRadius="$2"
+      borderTopLeftRadius={0}
+      borderTopRightRadius={0}
+      borderWidth="$2"
+      {...props}
+    >
+      {props.children}
+    </Tabs.Content>
+  );
+};
